e1657d200ad50ed6af91eeb941d67632fd67d213
Abhishek Sharma <sharmaabishek034@gmail.com>
2024-01-02 14:55:21 +0530

    try again added

--- src/Container.jsx
+++ src/Container.jsx
@@ -5,6 +5,7 @@ import {Cards} from "./Cards";
 import Filter from "./Filter";
 import Loader from "./Loader";
 import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 
 
@@ -12,12 +13,13 @@ export const ApplyFilter =  createContext(undefined);
 
 export default function Container()
 {
+    let url = useParams();
    const {mode,countryData,errorData,search,select,population,area,subregiondata,setCountryData,setError,setSearch,setSelect,setPopulation,setArea,setSubregiondata} = useContext(Darkmodecontext)
    const [loadedData,setLoadedData] = useState(['not']);
     useEffect(()=>{
         fetchData()
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[])
+    },[url])
 
   async function fetchData(){
     try {
@@ -33,6 +35,19 @@ export default function Container()
        setError(e);
     }
 }
+if(errorData)
+    {
+        return  (
+            <ApplyFilter.Provider value={{countryData ,select, setSearch ,setArea ,setSelect ,setPopulation,setSubregiondata}}>
+            <div   className={"container " + mode.bodyMode}>
+            <Filter/>
+             <h1 className={"no-country-found  " + mode.bodyMode} style={{display:"flex",flexDirection:"column" ,alignItems:'center' , rowGap:'50px'}}>{errorData.message} .Please try again <br /> <Link to="/" ><button  onClick={()=>{
+                setError('');
+             }} className={"back" + mode.elementMode} style={{margin:'0px auto'}}>Try Again</button></Link></h1>
+           </div>
+           </ApplyFilter.Provider>
+        )
+    }
 
 let filterData = countryData;
 if(countryData.length!=0)
@@ -52,20 +67,7 @@ if(countryData.length!=0)
     
     )
    }
-      
-    if(errorData)
-    {
-        return  (
-            <ApplyFilter.Provider value={{countryData ,select, setSearch ,setArea ,setSelect ,setPopulation,setSubregiondata}}>
-            <div   className={"container " + mode.bodyMode}>
-            <Filter/>
-             <h1 className={"no-country-found  " + mode.bodyMode} style={{display:"flex",flexDirection:"column" ,alignItems:'center' , rowGap:'50px'}}>something went wrong .Please try again <br /> <button id="back"  className={mode.elementMode} style={{margin:'0px auto'}}>Try Again</button></h1>
-           </div>
-           </ApplyFilter.Provider>
-        )
-    }
-
-    else if(countryData.length == 0)
+    if(countryData.length == 0)
     {
         return (
             <ApplyFilter.Provider value={{countryData ,select, setSearch ,setArea ,setSelect ,setPopulation,setSubregiondata}}>
